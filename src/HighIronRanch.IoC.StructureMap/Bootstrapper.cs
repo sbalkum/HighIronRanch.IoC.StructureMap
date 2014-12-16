418e8eaec2e396b8aaaa8e45f29c986d766ba88e
Stephen Balkum <stephen@balkum.com>
2014-12-16 16:06:35 -0600

    Initial code commit

---
+++ src/HighIronRanch.IoC.StructureMap/Bootstrapper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace HighIronRanch.IoC
+{
+	public class Bootstrapper : IBootstrapper
+	{
+		private readonly IEnumerable<string> _namespacesToScan;
+
+		public Bootstrapper(IEnumerable<string> namespacesToScan)
+		{
+			_namespacesToScan = namespacesToScan;
+		}
+
+		public void BootstrapStructureMap()
+		{
+			var container = new StructureMap.Container();
+
+			container.Configure(x => x.Scan(scanner =>
+			{
+				scanner.TheCallingAssembly();
+				//scanner.AssembliesFromApplicationBaseDirectory();
+				if (_namespacesToScan != null)
+				{
+					foreach (var name in _namespacesToScan)
+					{
+						scanner.AssembliesFromApplicationBaseDirectory(assembly => assembly.GetName().Name.StartsWith(name));
+					}
+				}
+
+				scanner.LookForRegistries();
+
+				scanner.RegisterConcreteTypesAgainstTheFirstInterface();
+
+				scanner.WithDefaultConventions();
+			}));
+
+			var whatIHave = container.WhatDoIHave();
+			//container.AssertConfigurationIsValid();
+
+			Container.TheContainer = container;
+		}
+
+		public static void Initialize()
+		{
+			Initialize(null);
+		}
+
+		public static void Initialize(IEnumerable<string> namespacesToScan)
+		{
+			new Bootstrapper(namespacesToScan).BootstrapStructureMap();
+		}
+	}
+}
---
+++ src/HighIronRanch.IoC.StructureMap/Container.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HighIronRanch.IoC
+{
+	public class Container
+    {
+	    public static StructureMap.Container TheContainer;
+
+		public static T GetInstance<T>()
+		{
+			return TheContainer.GetInstance<T>();
+		}
+
+		public static object GetInstance(Type modelType)
+		{
+			return TheContainer.GetInstance(modelType);
+		}
+
+		public static T TryGetInstance<T>()
+		{
+			return TheContainer.TryGetInstance<T>();
+		}
+
+		public static object TryGetInstance(Type modelType)
+		{
+			return TheContainer.TryGetInstance(modelType);
+		}
+
+		public static IEnumerable<T> GetAllInstances<T>()
+		{
+			return TheContainer.GetAllInstances<T>();
+		}
+
+		public static void BuildUp(object target)
+		{
+			TheContainer.BuildUp(target);
+		}
+    }
+}
